dcd2881e068dedf36995e77499a6f54ef68af7be
AbiramMuth <160613649+AbiramMuth@users.noreply.github.com>
2025-03-18 16:45:00 +0100

    feat: Fin de l'étape 5.5 (v-for)

--- IntroductionVueJS/main.js
+++ IntroductionVueJS/main.js
@@ -10,9 +10,14 @@ const my_app = Vue.createApp({
     // données
         product: 'Boots',
         description: description,
-        image: './assets/images/socks_green.jpg',
-        SiteEtml: etml,
-        inStock: true, // booléen
+        image: './assets/images/socks_green.jpg', // emplacement de l'img
+        SiteEtml: etml,  // lien du site
+        inStock: false, // booléen
+        inventory: 8,
+        onSale: true,
+        details: ['50% coton','30% wool','20% polyester'],  // tableau
+        variants: [{id: 2234, color:'green'},{id:2235, color: 'blue'}],
+        sizes: ['S','M','L','XL'],
         }
     }
 });
