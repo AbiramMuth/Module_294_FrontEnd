5066dbb49a557a2bf8538e9fad14fff9924f9ad5
AbiramMuth <160613649+AbiramMuth@users.noreply.github.com>
2025-03-18 17:19:39 +0100

    feat: fin de l'étape 6.4

--- IntroductionVueJS/main.js
+++ IntroductionVueJS/main.js
@@ -6,18 +6,25 @@ const etml = 'https://www.etml.ch';
 const my_app = Vue.createApp({
     // objet
     data() {
- return{
-    // données
-        product: 'Boots',
-        description: description,
-        image: './assets/images/socks_green.jpg', // emplacement de l'img
-        SiteEtml: etml,  // lien du site
-        inStock: false, // booléen
-        inventory: 8,
-        onSale: true,
-        details: ['50% coton','30% wool','20% polyester'],  // tableau
-        variants: [{id: 2234, color:'green'},{id:2235, color: 'blue'}],
-        sizes: ['S','M','L','XL'],
+        return {
+            // données
+            product: 'Boots',
+            description: description,
+            image: './assets/images/socks_green.jpg', // emplacement de l'img
+            SiteEtml: etml,  // lien du site
+            inStock: false, // booléen
+            inventory: 8,
+            onSale: true,
+            details: ['50% coton', '30% wool', '20% polyester'],  // tableau
+            variants: [{ id: 2234, color: 'green' }, { id: 2235, color: 'blue' }],
+            sizes: ['S', 'M', 'L', 'XL'],
+            cart: 0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
         }
     }
+
 });
