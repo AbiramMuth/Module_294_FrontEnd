a250aea8e6a0f0c73d5030d48216572228774c0d
AbiramMuth <160613649+AbiramMuth@users.noreply.github.com>
2025-03-18 14:41:45 +0100

    feat: Fin de l'étape 2.6

---
+++ IntroductionVueJS/main.js
@@ -0,0 +1,10 @@
+const product = 'Socks';
+const description = "A vous de coder !!!!"
+// Création d'une application Vue
+const my_app = Vue.createApp({
+    data() {
+ return{
+        product: 'Boots'
+        }
+    }
+});
