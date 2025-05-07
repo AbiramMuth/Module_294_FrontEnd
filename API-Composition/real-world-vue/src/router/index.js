922c85eb204b418c81c69c33a0d57a4294251cdd
AbiramMuth <160613649+AbiramMuth@users.noreply.github.com>
2025-05-07 12:21:07 +0200

    feat: Ajouts du component Page

--- API-Composition/real-world-vue/src/router/index.js
+++ API-Composition/real-world-vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
+import Page from '../views/Page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'event-details', // nom de la route
       props: true, // donne accès aux props
       component: EventDetailsView,
+    },
+    {
+      path: '/page',
+      name: 'page', // nom de la route
+      component: Page,
     },
   ],
 })
---
+++ API-Composition/real-world-vue/src/views/Page.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>You are on page {{ $route.query.page }}</h1>
+</template>
+
+<style>
+
+</style>
